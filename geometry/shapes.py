8edd348535552388f48c27653a1405074a82622f
Sergey Grudinin <sgrudinin2016@yandex.ru>
2024-07-23 21:08:44 +0800

    init_rep

---
+++ geometry/shapes.py
@@ -0,0 +1,31 @@
+from abc import ABC, abstractmethod
+import math
+
+class Shape(ABC):
+    @abstractmethod
+    def area(self):
+        pass
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return math.pi * self.radius ** 2
+
+class Triangle(Shape):
+    def __init__(self, a, b, c):
+        self.a = a
+        self.b = b
+        self.c = c
+
+    def area(self):
+        s = (self.a + self.b + self.c) / 2
+        return math.sqrt(s * (s - self.a) * (s - self.b) * (s - self.c))
+
+    def right_triangle_checking(self):
+        sides = sorted([self.a, self.b, self.c])
+        return math.isclose(sides[0] ** 2 + sides[1] ** 2, sides[2] ** 2)
+
+def calculate_area(shape: Shape):
+    return shape.area()
